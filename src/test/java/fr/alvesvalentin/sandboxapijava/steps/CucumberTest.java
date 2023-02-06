0d5d823025ca3eb4d8b3c145cde89e235463f496
Valentin Alvès <valentin-alves@hotmail.com>
2023-02-06 15:49:54 +0100

    deleted unused files

--- src/test/java/fr/alvesvalentin/sandboxapijava/steps/CucumberTest.java
+++
@@ -1,9 +0,0 @@
-package fr.alvesvalentin.sandboxapijava.steps;
-
-import fr.alvesvalentin.sandboxapijava.steps.RecupererTousLesUtilisateursStep;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-
-public class CucumberTest extends HelloApplicationSteps {}
